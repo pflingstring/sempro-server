b13e43a5764d1ada021807f9ebd2f560dabd2e65
pflingstring <nzaporojan@gmail.com>
2016-02-07 18:17:57 +0100

    add user model

---
+++ src/sempro/models/user.clj
@@ -0,0 +1,32 @@
+(ns sempro.models.user
+  (:require
+    [sempro.db.core :as db]
+    [bouncer.validators :as v]
+    [bouncer.core :as b]
+    ))
+
+(defn validate-user [user]
+  "`user` must be a map
+  returns a vector with 2 elements
+  the first argument is `nil` if user is valid
+  else it is a map with the errors"
+  (b/validate user
+    :first_name v/required
+    :last_name  v/required
+    :email [v/email v/required]
+    :pass   v/required))
+
+(defn create-user [user & env]
+  "`user` must be a map
+  returns a vector with 2 elements [bool, {map}]
+  if user is valid: [true, {user}]
+  else:             [false, {validation-errors}]"
+  (let [parsed-user (validate-user user)
+        errors (first  parsed-user)
+        user   (second parsed-user)]
+    (if (= nil errors)
+      (if [= env "test"]
+        (do (db/create-user! user db/test-jdbc) [true user])
+        (do (db/create-user! user                true user)))
+      [false errors])))
+
--- test/sempro/test/db/user.clj
+++ test/sempro/test/db/user.clj
@@ -1,11 +1,23 @@
 (ns sempro.test.db.user
   (:require
-    [sempro.test.db.resources.user :as res]
     [sempro.db.core :as db]
+    [sempro.models.user :as user]
+    [sempro.db.migrations :refer [migrate]]
+    [sempro.test.db.resources.user :as res]
 
     [midje.sweet :refer :all]
     ))
 
-(facts "User Test"
-  (first (db/get-user {:id 5} db/test-jdbc)) => res/user-rand)
+;; execute query in test DB
+(def run
+  (fn [query param]
+    (query param db/test-jdbc)))
 
+(def drop-user-table (do (migrate ["rollback" "test"])
+                         (migrate ["migrate"  "test"])))
+
+(with-state-changes [(before :facts drop-user-table)]
+  (fact "User Test"
+    (fact "Add user to database"
+      (user/create-user res/user-harry db/test-jdbc) => [true, res/user-harry]
+      )))
