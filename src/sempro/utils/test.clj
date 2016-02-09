98e2a62ba63cdc21815d00be2b7f86467a07711a
pflingstring <nzaporojan@gmail.com>
2016-02-09 05:23:50 +0100

    refactoring

--- src/sempro/utils/test.clj
+++ src/sempro/utils/test.clj
@@ -5,10 +5,13 @@
 
 (def wrap-middlewares #(sempro.handler/app %))
 
+(defn ignore-key [data key]
+  (assoc data key 'IGNORE))
+
 (defn ignore-headers [request]
   "adds an 'IGNORE symbol for :headers"
-  (assoc (wrap-middlewares request)
-         :headers 'IGNORE))
+  (ignore-key (wrap-middlewares request)
+              :headers))
 
 (defn post-req [url body]
   (m/request :post url body))
--- test/sempro/test/db/user.clj
+++ test/sempro/test/db/user.clj
@@ -2,26 +2,25 @@
   (:require
     [sempro.models.user :refer [create-user]]
     [sempro.test.db.resources.user :as res]
-    [sempro.db.migrations :refer [migrate]]
     [sempro.db.core    :as db]
     [sempro.utils.test :as u]
     [cheshire.core     :as json]
     [midje.sweet :refer :all]
-    ))
+    [clojure.java.jdbc :as jdbc]))
 
 ;;
 ;; TESTS
 ;;
 (conman.core/with-transaction [t-conn db/conn]
   (jdbc/db-set-rollback-only! t-conn)
+
   (fact "..:: USER  TESTS ::.."
 
     (facts "create new user using models.user"
-        (fact "should successfully create a user"
-          (let [rand res/user-rand
-                user (do (create-user rand)
-                         (-> (db/get-user-by-email {:email (:email rand)})
-                             (first) (assoc :id 'IGNORE)))]
-            user) => contains res/user-rand))
-      )
-  )
+      (fact "should successfully create a user"
+        (let [rand res/user-rand
+              user (do (create-user rand)
+                       (-> (db/get-user-by-email {:email (:email rand)})
+                           (first) (u/ignore-key :id)))]
+          user) => contains res/user-rand))
+    ))
