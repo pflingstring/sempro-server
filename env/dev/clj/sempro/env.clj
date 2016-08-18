5eb62ea700006a27c74d478c4532680373e61556
pflingstring <nzaporojan@gmail.com>
2016-08-18 17:24:56 +0200

    rename project name in logs

--- env/dev/clj/sempro/env.clj
+++ env/dev/clj/sempro/env.clj
@@ -4,6 +4,6 @@
     [sempro.dev-middleware :refer [wrap-dev]]))
 
 (def defaults
-  {:init (fn [] (log/info "\n-=[guestbook started successfully using the development profile]=-"))
-   :stop (fn [] (log/info "\n-=[guestbook has shut down successfully]=-"))
+  {:init (fn [] (log/info "\n-=[sempro started successfully using the development profile]=-"))
+   :stop (fn [] (log/info "\n-=[sempro has shut down successfully]=-"))
    :middleware wrap-dev})
